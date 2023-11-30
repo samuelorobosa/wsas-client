ef79787875c4090837b1d59071617d4ed632b021
oscarnovember <ndukwe.peter@roqqu.com>
2023-11-30 10:14:31 +0100

    start profile page setup

--- src/modules/user/pages/ProfilePage/ProfilePage.jsx
+++ src/modules/user/pages/ProfilePage/ProfilePage.jsx
@@ -1,7 +1,76 @@
+/* eslint-disable react/display-name */
+import { useState } from 'react';
 import './ProfilePage.css';
 
 export default function ProfilePage() {
+  const [editMode, enableEditMode] = useState(false);
+  const [name] = useState('John Doe');
+  const [email] = useState('johndoe@gmail.com');
+
   return (
-    <div className="profile-page">PROFILE PAGE</div>
+    <>
+      <nav className="appbar">
+        <h1 className="appbar-title">Edit Profile</h1>
+      </nav>
+      <main className="profile-page">
+        <div className="name-listtile">
+          <figure className="avatar-wrap">
+            <img src="" alt="interesting things" className="avatar" />
+            <div className="listtile-content">
+              <h6 className="name">{name}</h6>
+              <p className="email">{email}</p>
+            </div>
+          </figure>
+        </div>
+        <section className="info-content">
+          {
+            editMode ? <ProfilePage.EditableForm /> : <ProfilePage.NonEditableForm />
+          }
+        </section>
+      </main>
+    </>
+  )
+}
+
+ProfilePage.EditableForm = () => {
+  return (
+    <div className="form-wrap">
+      <form action="" className="profile-form">
+        <div className="form-group-input">
+          <label htmlFor=""></label>
+          <div className="">
+            <figure>
+              <img src="" alt="" />
+            </figure>
+            <input type="text" />
+          </div>
+        </div>
+      </form>
+      <div className="actions">
+        <button className="cancel">Cancel</button>
+        <button className="save">Save Changes</button>
+      </div>
+    </div>
+  )
+}
+
+ProfilePage.NonEditableForm = () => {
+  return (
+    <div className="form-wrap">
+      <div action="" className="profile-form">
+        <div className="form-group-input">
+          <label htmlFor=""></label>
+          <div className="">
+            <figure>
+              <img src="" alt="" />
+            </figure>
+            <input type="text" />
+          </div>
+        </div>
+      </div>
+      <div className="actions">
+        <button className="edit">Edit Profile</button>
+      </div>
+    </div>
   )
 }
--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ import loginReducer from "./modules/auth/state/login-slice";
 import navDrawerReducer from "./modules/dashboard/state/nav-drawer-slice";
 import ordersReducer from "./modules/order-management/state/orders-slice";
 import registrationReducer from "./modules/auth/state/registration-slice";
+import profileReducer from "./modules/user/state/profile-slice";
 
 const store = configureStore({
   devTools: process.env.NODE_ENV != "production",
@@ -11,6 +12,7 @@ const store = configureStore({
     login: loginReducer,
     navDrawer: navDrawerReducer,
     orders: ordersReducer,
+    profile: profileReducer,
     registration: registrationReducer,
   },
 });
