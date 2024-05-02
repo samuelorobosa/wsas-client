5ced40f93ee219b2ffcf798243e5c48b54589862
“Samuel <amagbakhensamuel@gmail.com>
2024-05-02 12:08:08 +0100

    Chore: Updated folder structures, updated dirnames and filenames

--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-undef */
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./modules/auth/authSlice.js";
-import navDrawerReducer from "./modules/dashboard/state/nav-drawer-slice";
-import ordersReducer from "./modules/order-management/state/orders-slice";
-import profileReducer from "./modules/user/state/profile-slice";
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './modules/auth/authSlice.js';
+import navDrawerReducer from './modules/dashboard/state/nav-drawer-slice';
+import ordersReducer from './modules/order-management/state/orders-slice';
+import profileReducer from './modules/user/profileSlice.js';
 
 const store = configureStore({
-  devTools: process.env.NODE_ENV !== "production",
+  devTools: process.env.NODE_ENV !== 'production',
   reducer: {
     auth: authReducer,
     navDrawer: navDrawerReducer,
